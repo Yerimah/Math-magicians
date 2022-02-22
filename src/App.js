620825fe514266b58391b8bb6f2f3f1de157cf16
Yerimah <danielyerimah@yahoo.com>
2022-02-22 14:41:20 +0100

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom' 
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './Components/Calculator';
-import Header from './Components/Header'
-import Home from './Components/Home'
-import Quote from './Components/Quote'
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Quote from './Components/Quote';
 
 const App = () => (
   <div className="App">
     <>
-    <Header />
-        <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/calculator" element={<Calculator />} />
-           <Route path="/quote" element={<Quote />} />
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
     </>
   </div>
 );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,42 +2,41 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
-    const navItems = [
-        {
-            id: 1,
-            path: '/',
-            text: 'Home',
-        },
-        {
-            id: 2,
-            path: '/calculator',
-            text: 'Calculator',
-        },
-        {
-            id: 3,
-            path: '/quote',
-            text: 'Quote',
-        },
-    ];
+  const navItems = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 3,
+      path: '/quote',
+      text: 'Quote',
+    },
+  ];
 
-
-    return (
-        <header className="nav-head">
-            <h1 className="title">Math Magicians</h1>
-            <nav>
-                <ul className="menu">
-                    {
+  return (
+    <header className="nav-head">
+      <h1 className="title">Math Magicians</h1>
+      <nav>
+        <ul className="menu">
+          {
                         navItems.map((navItems) => (
-                            <li key={navItems.id}>
-                                <NavLink to={navItems.path} className="menulinks">{navItems.text}</NavLink>
-                            </li>
+                          <li key={navItems.id}>
+                            <NavLink to={navItems.path} className="menulinks">{navItems.text}</NavLink>
+                          </li>
                         ))
                     }
-                </ul>
-            </nav>
+        </ul>
+      </nav>
 
-        </header>
-    );
+    </header>
+  );
 };
 
 export default Header;
