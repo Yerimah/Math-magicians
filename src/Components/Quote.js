eb5ad0930f9d1f1a526402ffda076dd6a4fd4e42
Yerimah <danielyerimah@yahoo.com>
2022-02-22 12:55:04 +0100

    Install react router, add items to the navbar and routing pages

---
+++ src/Components/Quote.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Quote = () => (
+    <Main className="quote-box">
+        <p className="famous-quote">
+        On two occasions I have been asked, 'Pray, Mr. Babbage, if you put into the machine wrong figures, will the right answers come out?' 
+        I am not able rightly to apprehend the kind of confusion of ideas that could provoke such a question.
+        <b>
+        -- Charles Babbage
+        </b>
+        </p>
+    </Main>
+
+)
+
+export default Quote;
