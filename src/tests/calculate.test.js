a0eb58f2d83d5a3f629ff5c91c0919815297ed55
Yerimah <danielyerimah@yahoo.com>
2022-02-28 09:33:40 +0100

    test components

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,12 @@
+import calculate from '../logic/calculate'
+
+describe('calculate.js testing', () => {
+    test('click on the first number', () => {
+            const obj = {
+                total: null,
+                next: null,
+                operation: null,
+            } 
+
+    })
+});
